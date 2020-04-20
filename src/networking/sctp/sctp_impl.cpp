f93438b2234c68d836784d33014858b4a0f53196
Prasanth Prahladan <prpr2770@colorado.edu>
2020-04-20 15:36:02 -0600

    add sctp_app

---
+++ src/networking/sctp/sctp_impl.cpp
@@ -0,0 +1,711 @@
+#include "sctp_impl.h"
+
+#include <sstream>
+#include <algorithm>
+
+#include <iostream>
+#include <stdlib.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/stat.h>
+#include <fcntl.h>
+#ifdef _WIN32
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <io.h>
+#define isatty _isatty
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> 
+#include <sys/select.h>
+#include <unistd.h>
+#define _open open
+#define _dup2 dup2
+#endif
+#include <string.h>
+#include <stdio.h>
+#include <string>
+#if __cplusplus < 201103L
+#else
+#include <cstdint>
+#endif
+typedef sctp_impl ivy_class;
+std::ofstream __ivy_out;
+std::ofstream __ivy_modelfile;
+void __ivy_exit(int code){exit(code);}
+
+class reader {
+public:
+    virtual int fdes() = 0;
+    virtual void read() = 0;
+    virtual void bind() {}
+    virtual bool running() {return fdes() >= 0;}
+    virtual ~reader() {}
+};
+
+class timer {
+public:
+    virtual int ms_delay() = 0;
+    virtual void timeout(int) = 0;
+    virtual ~timer() {}
+};
+
+#ifdef _WIN32
+DWORD WINAPI ReaderThreadFunction( LPVOID lpParam ) 
+{
+    reader *cr = (reader *) lpParam;
+    cr->bind();
+    while (true)
+        cr->read();
+    return 0;
+} 
+
+DWORD WINAPI TimerThreadFunction( LPVOID lpParam ) 
+{
+    timer *cr = (timer *) lpParam;
+    while (true) {
+        int ms = cr->ms_delay();
+        Sleep(ms);
+        cr->timeout(ms);
+    }
+    return 0;
+} 
+#else
+void * _thread_reader(void *rdr_void) {
+    reader *rdr = (reader *) rdr_void;
+    rdr->bind();
+    while(rdr->running()) {
+        rdr->read();
+    }
+    delete rdr;
+    return 0; // just to stop warning
+}
+
+void * _thread_timer( void *tmr_void ) 
+{
+    timer *tmr = (timer *) tmr_void;
+    while (true) {
+        int ms = tmr->ms_delay();
+        struct timespec ts;
+        ts.tv_sec = ms/1000;
+        ts.tv_nsec = (ms % 1000) * 1000000;
+        nanosleep(&ts,NULL);
+        tmr->timeout(ms);
+    }
+    return 0;
+} 
+#endif 
+
+#ifdef _WIN32
+    void sctp_impl::__lock() { WaitForSingleObject(mutex,INFINITE); }
+    void sctp_impl::__unlock() { ReleaseMutex(mutex); }
+#else
+    void sctp_impl::__lock() { pthread_mutex_lock(&mutex); }
+    void sctp_impl::__unlock() { pthread_mutex_unlock(&mutex); }
+#endif
+
+/*++
+Copyright (c) Microsoft Corporation
+
+This string hash function is borrowed from Microsoft Z3
+(https://github.com/Z3Prover/z3). 
+
+--*/
+
+
+#define mix(a,b,c)              \
+{                               \
+  a -= b; a -= c; a ^= (c>>13); \
+  b -= c; b -= a; b ^= (a<<8);  \
+  c -= a; c -= b; c ^= (b>>13); \
+  a -= b; a -= c; a ^= (c>>12); \
+  b -= c; b -= a; b ^= (a<<16); \
+  c -= a; c -= b; c ^= (b>>5);  \
+  a -= b; a -= c; a ^= (c>>3);  \
+  b -= c; b -= a; b ^= (a<<10); \
+  c -= a; c -= b; c ^= (b>>15); \
+}
+
+#ifndef __fallthrough
+#define __fallthrough
+#endif
+
+namespace hash_space {
+
+// I'm using Bob Jenkin's hash function.
+// http://burtleburtle.net/bob/hash/doobs.html
+unsigned string_hash(const char * str, unsigned length, unsigned init_value) {
+    register unsigned a, b, c, len;
+
+    /* Set up the internal state */
+    len = length;
+    a = b = 0x9e3779b9;  /* the golden ratio; an arbitrary value */
+    c = init_value;      /* the previous hash value */
+
+    /*---------------------------------------- handle most of the key */
+    while (len >= 12) {
+        a += reinterpret_cast<const unsigned *>(str)[0];
+        b += reinterpret_cast<const unsigned *>(str)[1];
+        c += reinterpret_cast<const unsigned *>(str)[2];
+        mix(a,b,c);
+        str += 12; len -= 12;
+    }
+
+    /*------------------------------------- handle the last 11 bytes */
+    c += length;
+    switch(len) {        /* all the case statements fall through */
+    case 11: 
+        c+=((unsigned)str[10]<<24);
+        __fallthrough;
+    case 10: 
+        c+=((unsigned)str[9]<<16);
+        __fallthrough;
+    case 9 : 
+        c+=((unsigned)str[8]<<8);
+        __fallthrough;
+        /* the first byte of c is reserved for the length */
+    case 8 : 
+        b+=((unsigned)str[7]<<24);
+        __fallthrough;
+    case 7 : 
+        b+=((unsigned)str[6]<<16);
+        __fallthrough;
+    case 6 : 
+        b+=((unsigned)str[5]<<8);
+        __fallthrough;
+    case 5 : 
+        b+=str[4];
+        __fallthrough;
+    case 4 : 
+        a+=((unsigned)str[3]<<24);
+        __fallthrough;
+    case 3 : 
+        a+=((unsigned)str[2]<<16);
+        __fallthrough;
+    case 2 : 
+        a+=((unsigned)str[1]<<8);
+        __fallthrough;
+    case 1 : 
+        a+=str[0];
+        __fallthrough;
+        /* case 0: nothing left to add */
+    }
+    mix(a,b,c);
+    /*-------------------------------------------- report the result */
+    return c;
+}
+
+}
+
+
+
+
+struct ivy_value {
+    int pos;
+    std::string atom;
+    std::vector<ivy_value> fields;
+    bool is_member() const {
+        return atom.size() && fields.size();
+    }
+};
+struct deser_err {
+};
+
+struct ivy_ser {
+    virtual void  set(long long) = 0;
+    virtual void  set(bool) = 0;
+    virtual void  setn(long long inp, int len) = 0;
+    virtual void  set(const std::string &) = 0;
+    virtual void  open_list(int len) = 0;
+    virtual void  close_list() = 0;
+    virtual void  open_list_elem() = 0;
+    virtual void  close_list_elem() = 0;
+    virtual void  open_struct() = 0;
+    virtual void  close_struct() = 0;
+    virtual void  open_field(const std::string &) = 0;
+    virtual void  close_field() = 0;
+    virtual void  open_tag(int, const std::string &) {throw deser_err();}
+    virtual void  close_tag() {}
+    virtual ~ivy_ser(){}
+};
+struct ivy_binary_ser : public ivy_ser {
+    std::vector<char> res;
+    void setn(long long inp, int len) {
+        for (int i = len-1; i >= 0 ; i--)
+            res.push_back((inp>>(8*i))&0xff);
+    }
+    void set(long long inp) {
+        setn(inp,sizeof(long long));
+    }
+    void set(bool inp) {
+        set((long long)inp);
+    }
+    void set(const std::string &inp) {
+        for (unsigned i = 0; i < inp.size(); i++)
+            res.push_back(inp[i]);
+        res.push_back(0);
+    }
+    void open_list(int len) {
+        set((long long)len);
+    }
+    void close_list() {}
+    void open_list_elem() {}
+    void close_list_elem() {}
+    void open_struct() {}
+    void close_struct() {}
+    virtual void  open_field(const std::string &) {}
+    void close_field() {}
+    virtual void  open_tag(int tag, const std::string &) {
+        set((long long)tag);
+    }
+    virtual void  close_tag() {}
+};
+
+struct ivy_deser {
+    virtual void  get(long long&) = 0;
+    virtual void  get(std::string &) = 0;
+    virtual void  getn(long long &res, int bytes) = 0;
+    virtual void  open_list() = 0;
+    virtual void  close_list() = 0;
+    virtual bool  open_list_elem() = 0;
+    virtual void  close_list_elem() = 0;
+    virtual void  open_struct() = 0;
+    virtual void  close_struct() = 0;
+    virtual void  open_field(const std::string &) = 0;
+    virtual void  close_field() = 0;
+    virtual int   open_tag(const std::vector<std::string> &) {throw deser_err();}
+    virtual void  close_tag() {}
+    virtual void  end() = 0;
+    virtual ~ivy_deser(){}
+};
+
+struct ivy_binary_deser : public ivy_deser {
+    std::vector<char> inp;
+    int pos;
+    std::vector<int> lenstack;
+    ivy_binary_deser(const std::vector<char> &inp) : inp(inp),pos(0) {}
+    virtual bool more(unsigned bytes) {return inp.size() >= pos + bytes;}
+    virtual bool can_end() {return pos == inp.size();}
+    void get(long long &res) {
+       getn(res,8);
+    }
+    void getn(long long &res, int bytes) {
+        if (!more(bytes))
+            throw deser_err();
+        res = 0;
+        for (int i = 0; i < bytes; i++)
+            res = (res << 8) | (((long long)inp[pos++]) & 0xff);
+    }
+    void get(std::string &res) {
+        while (more(1) && inp[pos]) {
+//            if (inp[pos] == '"')
+//                throw deser_err();
+            res.push_back(inp[pos++]);
+        }
+        if(!(more(1) && inp[pos] == 0))
+            throw deser_err();
+        pos++;
+    }
+    void open_list() {
+        long long len;
+        get(len);
+        lenstack.push_back(len);
+    }
+    void close_list() {
+        lenstack.pop_back();
+    }
+    bool open_list_elem() {
+        return lenstack.back();
+    }
+    void close_list_elem() {
+        lenstack.back()--;
+    }
+    void open_struct() {}
+    void close_struct() {}
+    virtual void  open_field(const std::string &) {}
+    void close_field() {}
+    int open_tag(const std::vector<std::string> &tags) {
+        long long res;
+        get(res);
+        if (res >= tags.size())
+            throw deser_err();
+        return res;
+    }
+    void end() {
+        if (!can_end())
+            throw deser_err();
+    }
+};
+struct ivy_socket_deser : public ivy_binary_deser {
+      int sock;
+    public:
+      ivy_socket_deser(int sock, const std::vector<char> &inp)
+          : ivy_binary_deser(inp), sock(sock) {}
+    virtual bool more(unsigned bytes) {
+        while (inp.size() < pos + bytes) {
+            int oldsize = inp.size();
+            int get = pos + bytes - oldsize;
+            get = (get < 1024) ? 1024 : get;
+            inp.resize(oldsize + get);
+            int newbytes;
+	    if ((newbytes = read(sock,&inp[oldsize],get)) < 0)
+		 { std::cerr << "recvfrom failed\n"; exit(1); }
+            inp.resize(oldsize + newbytes);
+            if (newbytes == 0)
+                 return false;
+        }
+        return true;
+    }
+    virtual bool can_end() {return true;}
+};
+
+struct out_of_bounds {
+    std::string txt;
+    int pos;
+    out_of_bounds(int _idx, int pos = 0) : pos(pos){
+        std::ostringstream os;
+        os << "argument " << _idx+1;
+        txt = os.str();
+    }
+    out_of_bounds(const std::string &s, int pos = 0) : txt(s), pos(pos) {}
+};
+
+template <class T> T _arg(std::vector<ivy_value> &args, unsigned idx, long long bound);
+template <class T> T __lit(const char *);
+
+template <>
+bool _arg<bool>(std::vector<ivy_value> &args, unsigned idx, long long bound) {
+    if (!(args[idx].atom == "true" || args[idx].atom == "false") || args[idx].fields.size())
+        throw out_of_bounds(idx,args[idx].pos);
+    return args[idx].atom == "true";
+}
+
+template <>
+int _arg<int>(std::vector<ivy_value> &args, unsigned idx, long long bound) {
+    std::istringstream s(args[idx].atom.c_str());
+    s.unsetf(std::ios::dec);
+    s.unsetf(std::ios::hex);
+    s.unsetf(std::ios::oct);
+    long long res;
+    s  >> res;
+    // int res = atoi(args[idx].atom.c_str());
+    if (bound && (res < 0 || res >= bound) || args[idx].fields.size())
+        throw out_of_bounds(idx,args[idx].pos);
+    return res;
+}
+
+template <>
+long long _arg<long long>(std::vector<ivy_value> &args, unsigned idx, long long bound) {
+    std::istringstream s(args[idx].atom.c_str());
+    s.unsetf(std::ios::dec);
+    s.unsetf(std::ios::hex);
+    s.unsetf(std::ios::oct);
+    long long res;
+    s  >> res;
+//    long long res = atoll(args[idx].atom.c_str());
+    if (bound && (res < 0 || res >= bound) || args[idx].fields.size())
+        throw out_of_bounds(idx,args[idx].pos);
+    return res;
+}
+
+template <>
+unsigned long long _arg<unsigned long long>(std::vector<ivy_value> &args, unsigned idx, long long bound) {
+    std::istringstream s(args[idx].atom.c_str());
+    s.unsetf(std::ios::dec);
+    s.unsetf(std::ios::hex);
+    s.unsetf(std::ios::oct);
+    unsigned long long res;
+    s  >> res;
+//    unsigned long long res = atoll(args[idx].atom.c_str());
+    if (bound && (res < 0 || res >= bound) || args[idx].fields.size())
+        throw out_of_bounds(idx,args[idx].pos);
+    return res;
+}
+
+template <>
+unsigned _arg<unsigned>(std::vector<ivy_value> &args, unsigned idx, long long bound) {
+    std::istringstream s(args[idx].atom.c_str());
+    s.unsetf(std::ios::dec);
+    s.unsetf(std::ios::hex);
+    s.unsetf(std::ios::oct);
+    unsigned res;
+    s  >> res;
+//    unsigned res = atoll(args[idx].atom.c_str());
+    if (bound && (res < 0 || res >= bound) || args[idx].fields.size())
+        throw out_of_bounds(idx,args[idx].pos);
+    return res;
+}
+
+
+std::ostream &operator <<(std::ostream &s, const __strlit &t){
+    s << "\"" << t.c_str() << "\"";
+    return s;
+}
+
+template <>
+__strlit _arg<__strlit>(std::vector<ivy_value> &args, unsigned idx, long long bound) {
+    if (args[idx].fields.size())
+        throw out_of_bounds(idx,args[idx].pos);
+    return args[idx].atom;
+}
+
+template <class T> void __ser(ivy_ser &res, const T &inp);
+
+template <>
+void __ser<int>(ivy_ser &res, const int &inp) {
+    res.set((long long)inp);
+}
+
+template <>
+void __ser<long long>(ivy_ser &res, const long long &inp) {
+    res.set(inp);
+}
+
+template <>
+void __ser<unsigned long long>(ivy_ser &res, const unsigned long long &inp) {
+    res.set((long long)inp);
+}
+
+template <>
+void __ser<unsigned>(ivy_ser &res, const unsigned &inp) {
+    res.set((long long)inp);
+}
+
+template <>
+void __ser<bool>(ivy_ser &res, const bool &inp) {
+    res.set(inp);
+}
+
+template <>
+void __ser<__strlit>(ivy_ser &res, const __strlit &inp) {
+    res.set(inp);
+}
+
+template <class T> void __deser(ivy_deser &inp, T &res);
+
+template <>
+void __deser<int>(ivy_deser &inp, int &res) {
+    long long temp;
+    inp.get(temp);
+    res = temp;
+}
+
+template <>
+void __deser<long long>(ivy_deser &inp, long long &res) {
+    inp.get(res);
+}
+
+template <>
+void __deser<unsigned long long>(ivy_deser &inp, unsigned long long &res) {
+    long long temp;
+    inp.get(temp);
+    res = temp;
+}
+
+template <>
+void __deser<unsigned>(ivy_deser &inp, unsigned &res) {
+    long long temp;
+    inp.get(temp);
+    res = temp;
+}
+
+template <>
+void __deser<__strlit>(ivy_deser &inp, __strlit &res) {
+    inp.get(res);
+}
+
+template <>
+void __deser<bool>(ivy_deser &inp, bool &res) {
+    long long thing;
+    inp.get(thing);
+    res = thing;
+}
+
+class gen;
+
+
+template <class T> void __from_solver( gen &g, const  z3::expr &v, T &res);
+
+template <>
+void __from_solver<int>( gen &g, const  z3::expr &v, int &res) {
+    res = g.eval(v);
+}
+
+template <>
+void __from_solver<long long>( gen &g, const  z3::expr &v, long long &res) {
+    res = g.eval(v);
+}
+
+template <>
+void __from_solver<unsigned long long>( gen &g, const  z3::expr &v, unsigned long long &res) {
+    res = g.eval(v);
+}
+
+template <>
+void __from_solver<unsigned>( gen &g, const  z3::expr &v, unsigned &res) {
+    res = g.eval(v);
+}
+
+template <>
+void __from_solver<bool>( gen &g, const  z3::expr &v, bool &res) {
+    res = g.eval(v);
+}
+
+template <>
+void __from_solver<__strlit>( gen &g, const  z3::expr &v, __strlit &res) {
+    res = g.eval_string(v);
+}
+
+template <class T>
+class to_solver_class {
+};
+
+template <class T> z3::expr __to_solver( gen &g, const  z3::expr &v, T &val) {
+    return to_solver_class<T>()(g,v,val);
+}
+
+
+template <>
+z3::expr __to_solver<int>( gen &g, const  z3::expr &v, int &val) {
+    return v == g.int_to_z3(v.get_sort(),val);
+}
+
+template <>
+z3::expr __to_solver<long long>( gen &g, const  z3::expr &v, long long &val) {
+    return v == g.int_to_z3(v.get_sort(),val);
+}
+
+template <>
+z3::expr __to_solver<unsigned long long>( gen &g, const  z3::expr &v, unsigned long long &val) {
+    return v == g.int_to_z3(v.get_sort(),val);
+}
+
+template <>
+z3::expr __to_solver<unsigned>( gen &g, const  z3::expr &v, unsigned &val) {
+    return v == g.int_to_z3(v.get_sort(),val);
+}
+
+template <>
+z3::expr __to_solver<bool>( gen &g, const  z3::expr &v, bool &val) {
+    return v == g.int_to_z3(v.get_sort(),val);
+}
+
+template <>
+z3::expr __to_solver<__strlit>( gen &g, const  z3::expr &v, __strlit &val) {
+//    std::cout << v << ":" << v.get_sort() << std::endl;
+    return v == g.int_to_z3(v.get_sort(),val);
+}
+
+template <class T>
+class __random_string_class {
+public:
+    std::string operator()() {
+        std::string res;
+        res.push_back('a' + (rand() % 26)); // no empty strings for now
+        while (rand() %2)
+            res.push_back('a' + (rand() % 26));
+        return res;
+    }
+};
+
+template <class T> std::string __random_string(){
+    return __random_string_class<T>()();
+}
+
+template <class T> void __randomize( gen &g, const  z3::expr &v);
+
+template <>
+void __randomize<int>( gen &g, const  z3::expr &v) {
+    g.randomize(v);
+}
+
+template <>
+void __randomize<long long>( gen &g, const  z3::expr &v) {
+    g.randomize(v);
+}
+
+template <>
+void __randomize<unsigned long long>( gen &g, const  z3::expr &v) {
+    g.randomize(v);
+}
+
+template <>
+void __randomize<unsigned>( gen &g, const  z3::expr &v) {
+    g.randomize(v);
+}
+
+template <>
+void __randomize<bool>( gen &g, const  z3::expr &v) {
+    g.randomize(v);
+}
+
+template <>
+        void __randomize<__strlit>( gen &g, const  z3::expr &apply_expr) {
+    z3::sort range = apply_expr.get_sort();
+    __strlit value = (rand() % 2) ? "a" : "b";
+    z3::expr val_expr = g.int_to_z3(range,value);
+    z3::expr pred = apply_expr == val_expr;
+    g.add_alit(pred);
+}
+
+template<typename D, typename R>
+class z3_thunk : public thunk<D,R> {
+    public:
+       virtual z3::expr to_z3(gen &g, const  z3::expr &v) = 0;
+};
+
+int sctp_impl::___ivy_choose(int rng,const char *name,int id) {
+        std::ostringstream ss;
+        ss << name << ':' << id;;
+        for (unsigned i = 0; i < ___ivy_stack.size(); i++)
+            ss << ':' << ___ivy_stack[i];
+        return ___ivy_gen->choose(rng,ss.str().c_str());
+    }
+void sctp_impl::__init(){
+    {
+    }
+}
+void sctp_impl::__tick(int __timeout){
+}
+sctp_impl::sctp_impl(){
+#ifdef _WIN32
+mutex = CreateMutex(NULL,FALSE,NULL);
+#else
+pthread_mutex_init(&mutex,NULL);
+#endif
+__lock();
+}
+sctp_impl::~sctp_impl(){
+    __lock(); // otherwise, thread may die holding lock!
+    for (unsigned i = 0; i < thread_ids.size(); i++){
+#ifdef _WIN32
+       // No idea how to cancel a thread on Windows. We just suspend it
+       // so it can't cause any harm as we destruct this object.
+       SuspendThread(thread_ids[i]);
+#else
+        pthread_cancel(thread_ids[i]);
+        pthread_join(thread_ids[i],NULL);
+#endif
+    }
+    __unlock();
+}
+init_gen::init_gen(){
+    mk_const("_generating","Bool");
+    add("(assert (and\
+      and\
+    ))");
+}
+bool init_gen::generate(sctp_impl& obj) {
+    alits.clear();
+    obj._generating = (bool)(rand() % 2);
+
+    // std::cout << slvr << std::endl;
+    bool __res = solve();
+    if (__res) {
+
+    }
+
+    obj.___ivy_gen = this;
+    obj.__init();
+    return __res;
+}
